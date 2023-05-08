b5ac712b7350904b43d53c95f585035b3610e1d0
Sonali Sahu <71460151+sonali375@users.noreply.github.com>
2023-05-08 11:13:45 +0530

    Add files via upload

--- calculator_using_class_method.py
+++ calculator_using_class_method.py
@@ -1,53 +1,54 @@
-class calculator():
+"""this is the calculator program"""
+
+
+class Calculator():
     """this is the class for calculator"""
-    def __init__(self,number_1,number_2):
-        self.number_1 = number_1
-        self.number_2 = number_2
+
+    def __init__(self, number1, number2):
+        self.number_1 = number1
+        self.number_2 = number2
+
     def add(self):
         """function for addition"""
         return self.number_1 + self.number_2
+
     def sub(self):
         """function for subtraction"""
         return self.number_1 - self.number_2
+
     def multiply(self):
         """function for multiplication"""
         return self.number_1 * self.number_2
+
     def divide(self):
         """function for division"""
         return self.number_1 / self.number_2
 
-number_1 = int(input('Enter First number : '))
-number_2 = int(input('Enter Second number : '))    
 
-obj=calculator(number_1,number_2)
+number_1 = int(input('Enter First number : '))
+number_2 = int(input('Enter Second number : '))
+obj = Calculator(number_1, number_2)
 """passing parameter to calculator"""
 
 while True:
     def menu():
-        option = ('1. Add \n2. Sub \n3. Multiply \n4. Divide \n5. Exit') 
+        """ function for showing the menu"""
+        option = '1. Add \n2. Sub \n3. Multiply \n4. Divide \n5. Exit'
         print(option)
-
     menu()
-    choice = int(input('Please select one of the following : ')) 
-    
+    choice = int(input('Please select one of the following : '))
     if choice == 1:
-        """perform addition"""
-        print("Result1: ",obj.add())
+        print("Result1: ", obj.add())
     elif choice == 2:
-        """perform subtraction"""
-        print("Result2: ",obj.sub())
+        print("Result2: ", obj.sub())
     elif choice == 3:
-        """perform multiplication"""
-        print("Result3: ",obj.multiply())    
+        print("Result3: ", obj.multiply())
     elif choice == 4:
-        """perform division"""
-        print("Result4: ",obj.divide())
+        print("Result4: ", obj.divide())
     elif choice == 5:
-        """to exit from the options"""
         print('Thank You')
         break
     else:
-        """any other value than the options available is invalid"""
         print('Invalid option')
         break
 print()
